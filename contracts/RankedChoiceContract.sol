603b0f8e2b0e9347143f761aab219b20cb7fd94b
Kent Miguel <kentmiguel10@gmail.com>
2022-11-14 20:50:20 +0100

    Create enterCandidate function

---
+++ contracts/RankedChoiceContract.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/// Errors ///
+error Voting_CandidateAlreadyExists(address candidateAddress);
+error Voting_CandidateAddressDoesNotExist(address candidateAddress);
+
+/**
+ * @title A Ranked Choice Voting Smart Contract
+ * @author Kent Miguel
+ * @dev uses chainlink automation to end register, voting, and count phases
+ */
+contract RankedChoiceContract {
+    using Counters for Counters.Counter;
+
+    //Events
+    event CandidateCreated(
+        uint256 indexed id,
+        string indexed name,
+        address indexed walletAddress
+    );
+
+    /// Candidate variables ///
+    struct Candidate {
+        uint256 id;
+        string name;
+        address walletAddress;
+        uint256 firstVotesCount;
+        uint256 secondVotesCount;
+        uint256 thirdVotesCount;
+        bool isEliminated;
+        bool isWinner;
+        uint256 totalVotesCount;
+    }
+
+    address[] private candidateAddresses; //array of candidates
+    //mapping(uint256 => mapping(address => Candidate)) private addressToCandidate;
+    mapping(address => Candidate) private addressToCandidate;
+
+    /// Voter variables ///
+
+    /// Election global variables ///
+    uint256 numberOfCandidates;
+    bool isWinnerPicked;
+    Counters.Counter private candidateIdCounter;
+
+    //mapping candidate address to struct
+
+    //create a constructor to start timer for phases
+    //must initialize the state of voting
+
+    /** Functions */
+
+    //Function to register
+    function enterCandidate(
+        // address _candidateAddress,
+        string memory _candidateName // uint256 _firstVotesCount, // uint256 _secondVotesCount, // uint256 _thirdVotesCount
+    ) external {
+        // checks:
+        //if candidate already exists
+
+        //assign candidate id number
+        candidateIdCounter.increment();
+        uint256 _candidateId = candidateIdCounter.current();
+
+        //create candidate struct
+        // Candidate memory candidate = addressToCandidate[_candidateAddress];
+        Candidate memory candidate = Candidate(
+            _candidateId,
+            _candidateName,
+            // _candidateAddress,
+            msg.sender,
+            0,
+            0,
+            0,
+            false,
+            false,
+            0
+        );
+
+        // candidate.id = _candidateId;
+        // candidate.name = _candidateName;
+        // candidate.walletAddress = _candidateAddress;
+        // candidate.firstVotesCount = _firstVotesCount;
+        // candidate.secondVotesCount = _secondVotesCount;
+        // candidate.thirdVotesCount = _thirdVotesCount;
+        // candidate.isEliminated = false;
+        // candidate.isWinner = false;
+        // candidate.totalVotesCount = 0;
+
+        //store candidate struct in mapping
+        addressToCandidate[msg.sender] = candidate;
+
+        //push to candidate address
+        candidateAddresses.push(msg.sender);
+
+        //emit CandidateRegistered event
+        emit CandidateCreated(
+            candidate.id,
+            candidate.name,
+            candidate.walletAddress
+        );
+    }
+
+    /// Getter functions ///abi
+    function getCandidateByAddress(address _candidateAddress)
+        external
+        view
+        returns (Candidate memory)
+    {
+        if (addressToCandidate[_candidateAddress].id <= 0) {
+            revert Voting_CandidateAddressDoesNotExist(_candidateAddress);
+        }
+        return addressToCandidate[_candidateAddress];
+    }
+}
---
+++ test/RankedChoiceTest.ts
@@ -0,0 +1,5 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+import { expect, assert } from "chai"
+import { ethers } from "hardhat"
+
+describe("RankedChoiceVoting", function () {})
