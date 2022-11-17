b4d8c696e0dd6c57fe6e0fd2be5c89cab8755624
Kent Miguel <kentmiguel10@gmail.com>
2022-11-17 23:40:52 +0100

    Added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import "dotenv/config"
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-etherscan"
 import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
@@ -46,6 +47,9 @@ const config: HardhatUserConfig = {
             default: 0, // here this will by default take the first account as deployer
             1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
+    },
+    gasReporter: {
+        enabled: true,
     },
 }
 
